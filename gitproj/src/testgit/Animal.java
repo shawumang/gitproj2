41d51a3f4039f60e22bb9e12ecf7a93b83f8af71
Nagendra Sajwan <nsajwan@gmail.com>
2019-06-17 15:08:56 +0530

    animal details

---
+++ gitproj/src/testgit/Animal.java
@@ -0,0 +1,10 @@
+package testgit;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("importing this class to git");
+	}
+
+}
