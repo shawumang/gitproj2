de0f182b791d45016811d02108df17c1f8c5e40d
Umang Shaw <51898627+shawumang@users.noreply.github.com>
2019-06-17 15:18:57 +0530

    Update Animal.java

--- gitproj/src/testgit/Animal.java
+++ gitproj/src/testgit/Animal.java
@@ -5,6 +5,7 @@ public class Animal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("importing this class to git");
+		System.out.println("added line 2 in this animal class");
 	}
 
 }
